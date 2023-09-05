752c370ec2993801656bc105687e14c1fe8a5858
hectorHumanDev <113396173+MF-H0d@users.noreply.github.com>
2023-09-05 16:22:30 -0700

    added button press/click animation

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ for (let i = 0; i < numberOfButtons; i++) {
 
         var buttonInnerHTML = this.innerHTML
         makeSound(buttonInnerHTML)
+        buttonAnimation(buttonInnerHTML)
     })
 
 }
@@ -12,6 +13,7 @@ for (let i = 0; i < numberOfButtons; i++) {
 
 document.addEventListener('keydown', function(event){
     makeSound(event.key)
+    buttonAnimation(event.key)
 })
 
 
@@ -50,4 +52,14 @@ function makeSound(key){
         default:console.log(buttonInnerHTML)
             break;
     }
+}
+
+function buttonAnimation(currentKey) {
+    let activeButton = document.querySelector("." + currentKey)
+    activeButton.classList.add('pressed')
+
+    setTimeout(function(){
+        activeButton.classList.remove('pressed')
+    }, 100)
+
 }
